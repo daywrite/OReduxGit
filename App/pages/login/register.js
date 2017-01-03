72de14f0afedbb8a24edfc51487c777b609174d5
李明 <liming@limingdeMacBook-Pro.local>
2017-01-03 13:14:52 +0800

    注册用户并登录

--- App/pages/login/register.js
+++ App/pages/login/register.js
@@ -17,13 +17,21 @@ import {GiftedForm, GiftedFormManager} from 'react-native-gifted-form';
 const USER = require('../../imgs/login/register-user-icon.png');
 const PWD = require('../../imgs/login/register-pwd-icon.png');
 import RegisterHeader from './registerheader';
+import {BASE_URL} from '../../common/Config';
+import HTTPUtil from '../../utils/HttpUtil';
+import {performLoginAction} from '../../Actions/LoginAction';
+import {connect} from 'react-redux';
 var winHeight = Dimensions.get('window').height;
-export default class Register extends Component {
+class Register extends Component {
     constructor(props) {
         super(props)
     }
     _preSubmit() {}
+    createUrl() {
+        return (BASE_URL + 'user/create');
+    }
     render() {
+        const {login} = this.props;
         return (
             <View style={styles.container}>
                 <RegisterHeader navigator={this.props.navigator}/>
@@ -68,7 +76,29 @@ export default class Register extends Component {
                         }
                     }} preSubmit={this._preSubmit.bind(this)} onSubmit={(isValid, values, validationResults, postSubmit = null, modalNavigator = null) => {
                         if (isValid === true) {
-                            this.props.navigator.pop();/*this.refs.toast.show("注册成功");*/
+                            var usernameVaule = GiftedFormManager.getValue('signupForm', 'username');
+                            var passwordVaule = GiftedFormManager.getValue('signupForm', 'password');
+                            let formData = {
+                                "UserName": usernameVaule,
+                                "PassWord": passwordVaule
+                            };
+                            let headers = {
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/json'
+                            };
+                            const {navigator, dispatch} = this.props;
+                            HTTPUtil.post(this.createUrl(), JSON.stringify(formData), headers).then((json) => {
+                                if (json.code === 0) {
+                                    this.refs.toast.show("注册成功");
+                                    dispatch(performLoginAction(usernameVaule, passwordVaule, navigator));
+                                } else {
+                                    console.log(json);
+                                }
+                            }, (json) => {
+                                console.log('falie');
+                                console.log(json);
+                            });/*this.props.navigator.pop();*/
+                            /*this.refs.toast.show("注册成功");*/
                         } else {
                             var errorList = validationResults.results;/*console.log(errorList);*/
                             var usernameVaule = GiftedFormManager.getValue('signupForm', 'username');
@@ -108,3 +138,8 @@ const styles = StyleSheet.create({
         height: winHeight
     }
 });
+function mapStateToProps(state) {
+    const {login} = state;
+    return {login}
+}
+export default connect(mapStateToProps)(Register);
